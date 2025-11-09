2e943bcd4b55086abbe56a939888ffa231bbd109
Nibin <87687660+nibinrj@users.noreply.github.com>
2025-11-09 17:05:06 +0530

    november commit

--- src/main/java/com/nibin/quiz/Model/Question.java
+++ src/main/java/com/nibin/quiz/Model/Question.java
@@ -2,9 +2,7 @@ package com.nibin.quiz.Model;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 import java.util.List;
 
